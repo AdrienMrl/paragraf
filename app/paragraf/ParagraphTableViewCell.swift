4356422c08ede64069f55717dd83e904cb129900
Adrien morel <adrien.morel@me.com>
2016-11-17 17:06:05 +0100

    finish first draft for story detail view

--- app/paragraf/ParagraphTableViewCell.swift
+++ app/paragraf/ParagraphTableViewCell.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ParagraphTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var authorProfileView: UIView!
     
     public func setParagraph(_ text: String) {
-        textView.text = text
+        
+        let style = NSMutableParagraphStyle()
+        
+        style.lineSpacing = 8
+        style.firstLineHeadIndent = 16
+        style.alignment = .justified
+        
+        
+        let attributes = [NSParagraphStyleAttributeName: style]
+        textView.attributedText = NSAttributedString(string: text, attributes:attributes)
+        
+        textView.font = UIFont(name: "Georgia", size: 18)
+        
+    }
+    
+    public func setProfile(/* pic: UIImage */) {
+        
+        profilePic.layer.cornerRadius = profilePic.frame.width / 2
+        profilePic.layer.masksToBounds = true
+    }
+    
+    override func awakeFromNib() {
+        
+        self.layoutIfNeeded()
+        
+        setProfile()
+        
+        
+        var rec = authorProfileView.frame
+//        rec = rec.applying(CGAffineTransform(translationX: 0, y: -40))
+        
+        let imgRect = UIBezierPath(rect: rec)
+        textView.textContainer.exclusionPaths = [imgRect]
     }
 }
--- app/paragraf/StoryDetailTableViewController.swift
+++ app/paragraf/StoryDetailTableViewController.swift
@@ -16,12 +16,36 @@ class StoryDetailTableViewController: UITableViewController {
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
-
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        
         self.tableView.estimatedRowHeight = 100
         self.tableView.rowHeight = UITableViewAutomaticDimension
         
+        UIApplication.shared.statusBarStyle = .lightContent
+        
+    }
+    @IBAction func publish(_ sender: Any) {
+        
+        
+        let alert = UIAlertController(title: "Publish", message: "Are you sure ?", preferredStyle: UIAlertControllerStyle.alert)
+        let alertAction = UIAlertAction(title: "Yes", style: UIAlertActionStyle.default)
+        {
+            (UIAlertAction) -> Void in
+            
+            print("commit")
+        }
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+        {
+            () -> Void in
+        }
+
     }
 
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -30,29 +54,38 @@ class StoryDetailTableViewController: UITableViewController {
         return 5
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        
-        let cell: UITableViewCell!
-        
-        if (indexPath.row == 0) {
-            
-            cell = tableView.dequeueReusableCell(withIdentifier: "headerCell", for: indexPath)// as! HeaderStoryDetailTableViewCell
-            
-        } else {
-            
-            let cellParagraph = tableView.dequeueReusableCell(withIdentifier: "paragraphCell", for: indexPath) as! ParagraphTableViewCell
-            
-            
-            let text = indexPath.row == 1 ? "Hi guys. I'm matt\nMatt" : "Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. Bite bien dure. "
-            
-            cellParagraph.setParagraph(text)
-            
-            cell = cellParagraph
-        }
-        
-        return cell
+    
+    func genHeader(for indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "headerCell", for: indexPath) as! StoryDetailHeaderTableViewCell
     }
     
-
+    func genParagraph(for indexPath: IndexPath) -> UITableViewCell {
+        let cellParagraph = tableView.dequeueReusableCell(withIdentifier: "paragraphCell", for: indexPath) as! ParagraphTableViewCell
+        
+        let text = indexPath.row == 1 ? "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda." : "Another pargrap Some text Some text Some text Some text Some text Some text Some text Some text Some text Some text "
+        
+        cellParagraph.setParagraph(text)
+        
+        return cellParagraph
+    }
+    
+    func genPrompt(for indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "prompt", for: indexPath)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if (indexPath.row == 0) {
+            return genHeader(for: indexPath)
+        } else if (indexPath.row == 4) {
+            return genPrompt(for: indexPath)
+        } else {
+            return genParagraph(for: indexPath)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("something seguing !")
+    }
+    
 }
