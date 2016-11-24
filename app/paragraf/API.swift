7dc8d071ba86c1e0615d181d4932e1006e146562
gwendal lasson <gwendal.lasson@gmail.com>
2016-11-24 12:18:34 +0100

    api class

--- app/paragraf/API.swift
+++ app/paragraf/API.swift
@@ -9,16 +9,35 @@
 import UIKit
 import Alamofire
 import FBSDKLoginKit
+import SwiftyJSON
 
 
 class API {
     
-    let server: String
+    let serverURL: String
     let port: Int
+    var API_token: String? = nil
     
-    init(server: String, port: Int) {
-        self.server = server
+    
+    init(serverURL: String, port: Int) {
+        self.serverURL = serverURL
         self.port = port
+        
     }
+    
+    func retrieveToken() -> Bool {
+        let parameters: Parameters = ["token" : FBSDKAccessToken.current().tokenString]
+        Alamofire.request(serverURL,method: .post, parameters: parameters).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                print("Token granted")
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
 
+        return API_token != nil
+    }
 }
