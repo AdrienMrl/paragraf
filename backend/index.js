c96a4422233503c7afbe21a5636145ed7778d4c4
Adrien morel <gwendal.lasson@gmail.com>
2016-11-27 19:25:15 +0100

    move to submodules

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const route = require('./route.js')
-
-route.startListening(3000)
-  .then(() => console.log('Welcome. Listening on port 3000. Happy writting :).'))
-  .catch(err => console.error(err))
